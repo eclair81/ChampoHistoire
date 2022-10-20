934ca83b3bc076f40de82fa5c437da180d9d5b50
eclair81 <bastienpic.bp@gmail.com>
2022-10-20 17:48:38 +0200

    {Update} GridGenerator Script
    Added a way to generate a custom shaped grid based on a texture2D.
    For now, only uses black pixels. More colors support can be added later to customise the type of tile to spawn.

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -9,10 +9,13 @@ public class GridGenerator : MonoBehaviour
     [SerializeField] private GameObject tilePrefab;
     [Header("Camera")]
     [SerializeField] private Transform cam;
+    [Header("Custom Grid")]
+    [SerializeField] private Texture2D map;
 
     void Start()
     {
-        GenerateGrid();
+        //GenerateGrid();
+        GenerateCustomGrid();
     }
 
     private void GenerateGrid()
@@ -30,4 +33,33 @@ public class GridGenerator : MonoBehaviour
         //Place camera at the center of the grid
         cam.transform.position = new Vector3((float)rows/2 - 0.5f, (float)cols/2 -0.5f, -10);
     }
+
+    //Generate a custom grid based on a Texture2D
+    private void GenerateCustomGrid(/*Texture2D map*/)
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                Color pixel = map.GetPixel(x, y);
+                //Debug.Log(pixel);
+                GenerateTile(x, y, pixel);
+            }
+        }
+    }
+
+    private void GenerateTile(int x, int y, Color pixel)
+    {
+        if(pixel.a == 0) return; //Ignore transparent pixels
+
+        //Only using black pixels for now
+        if(pixel.r == 0 && pixel.g == 0 && pixel.b == 0)
+        {
+            GameObject tile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+            Tile tileScript = tile.GetComponent<Tile>();
+            bool offset = ((x + y) % 2 == 1); //alternate for a checkboard grid
+            tileScript.Init(offset);
+        }
+    }
+
 }
