df74b0b8356f7a245279bc95bd4c2961cb72a22f
eclair81 <bastienpic.bp@gmail.com>
2022-10-21 22:18:02 +0200

    {Update} Tile Prefab & Script
    Added a BoxCollider2D to the prefab (for raycast).
    Added a function in the script to change the color when a tile is touched.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField] private Color color1, color2;
+    [SerializeField] private Color color1, color2, color3, color4;
     private SpriteRenderer spriteRenderer;
+    private bool alreadyInteracted;
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        alreadyInteracted = false;
     }
 
     public void Init(bool isOffset)
     {
         spriteRenderer.color = isOffset ? color1 : color2;
+    }
+
+    public void Interact()
+    {
+        if(alreadyInteracted) return;
+
+        spriteRenderer.color = (spriteRenderer.color == color1) ? color3 : color4;
+        alreadyInteracted = true;
     }
 }
